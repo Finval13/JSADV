55f903b7a6a89f89d0dc2b7f0cd95880b2d90e59
Андрій <FInval334@i.ua>
2023-10-21 17:31:45 +0300

    FInal

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// const div = document.querySelectorAll("img");
+// let arr = [];
+// div.forEach((elem) => (arr += " ; " + elem.src));
+// console.log(arr);
+
+// let arr2 = [];
+// div.forEach((elem) => (arr2 += " , " + elem.alt));
+// console.log(arr2);
+
+// const div = document.querySelectorAll("div");
+// let arr = [];
+// div.forEach((elem) => (arr += elem.className + "  , "));
+
+// console.log(arr);
+
+// const img = document.querySelectorAll("img");
+
+// img.forEach((image) => {
+//   const imss = image.getAttribute("Alt");
+//   if (imss) {
+//     image.setAttribute("title", imss);
+//   }
+// });
+
+// const div = document.querySelectorAll(".back");
+// div.forEach((elem) => (elem.style.backgroundColor = "#fff000"));
